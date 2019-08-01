d25886d3ff66bccda615c300fc8e40cccb979f89
wkj89 <wkj89@qq.com>
2019-08-01 10:57:24 +0800

    first try

--- entry_point.py
+++ entry_point.py
@@ -4,27 +4,30 @@
 import os
 import sys
 
-code_url = os.environ.get('code_url','')
-if code_url =="":
+code_url = os.environ.get('code_url', '')
+if code_url == "":
     print('code_url env not set')
     sys.exit()
-# code_url = "https://github.com/wkj89/hello-grpc/archive/master.zip"
+# code_url = "https://github.com/wkj89/auto-buld-debug/archive/master.zip"
 cmd = 'wget %s' % code_url
 tmp = os.popen(cmd).readlines()
 exists = os.path.exists("/master.zip")
 if not exists:
-    print("下载文件失败，url：%s", code_url)
+    print(u"download file err，url：%s" % code_url)
     print(tmp)
 
     sys.exit(1)
-os.popen("mkdir code ")
-tmp = os.popen("unzip  master.zip -d code ").readlines()
-dirs = os.listdir("/code")
+os.popen("mkdir code_tmp")
+tmp = os.popen("unzip  master.zip -d code_tmp ").readlines()
+dirs = os.listdir("/code_tmp")
 if len(dirs) == 0:
-    print('解压缩失败')
+    print(u'failed to unzip')
     print(tmp)
 
 dir_name = dirs[0]
 
-os.popen('./code/%s/entrypoint.sh', dir_name)
+os.popen("mv /code_tmp/%s /code" % dir_name)
+
+print(u"start run /code/entrypoint.sh ")
+os.popen("./code/entrypoint.sh")
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python a.py
+python /code/a.py
